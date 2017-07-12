9ede931a4f5f10551de7244194b68bdaad9c8539
tramontaz <Pir0jo4eK>
2017-07-12 11:55:27 +0300

    Add UPDATE method for update cost of the goods; Add SELECT method por select all goods by cost > x and < y;

--- src/main/java/ru/geekbrains/java3lesson2HW/Main.java
+++ src/main/java/ru/geekbrains/java3lesson2HW/Main.java
@@ -9,21 +9,92 @@ public class Main {
 
 
     public static void main(String[] args) {
+//        taskOneAndTwo();
+//        taskThree();
+//        taskFour();
+//        taskFive();
+    }
+
+    private static void taskFive() {
         try {
             connect();
             long t = System.currentTimeMillis();
-//            taskOne();
-            selectCostOfTheProduct();
-            System.out.println(System.currentTimeMillis() - t);
+            goodsByPrice(100, 150);
+            System.out.println("\n \n Lead time: " + (System.currentTimeMillis() - t));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException d) {
+            d.printStackTrace();
         } finally {
             disconnect();
         }
+    }
 
+    private static void goodsByPrice(double minPrice, double maxPrice) throws SQLException{
+        String sqlQuery = "SELECT id, title, cost FROM shop WHERE cost >= ? and cost <= ?;";
+        PreparedStatement psSelectGoodsByPrice = connection.prepareStatement(sqlQuery);
+        psSelectGoodsByPrice.setDouble(1, minPrice);
+        psSelectGoodsByPrice.setDouble(2, maxPrice);
+        ResultSet rs = psSelectGoodsByPrice.executeQuery();
+        while (rs.next()){
+            System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getDouble(3));
+        }
+    }
 
+    private static void taskFour() {
+        try {
+            connect();
+            long t = System.currentTimeMillis();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter title of product which you want to change: ");
+            String scanTitle = sc.nextLine();
+            System.out.println("Enter new cost: ");
+            int scanCost = sc.nextInt();
+            PreparedStatement psUpdate = connection.prepareStatement("UPDATE shop SET cost = ? WHERE title = ?");
+            psUpdate.setInt(1, scanCost);
+            psUpdate.setString(2, scanTitle);
+            psUpdate.executeUpdate();
+            sc.close();
+            System.out.println("\n \n Lead time: " + (System.currentTimeMillis() - t));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException d) {
+            System.out.println("Error: there is no product with this name");
+        } finally {
+            disconnect();
+        }
+    }
+
+    private static void taskOneAndTwo() {
+        try {
+            connect();
+            long t = System.currentTimeMillis();
+            dropTable();
+            createTable();
+            fillTable();
+            System.out.println("\n \n Lead time: " + (System.currentTimeMillis() - t));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException d) {
+            d.printStackTrace();
+        } finally {
+            disconnect();
+        }
+    }
+
+    private static void taskThree() {
+        try {
+            connect();
+            long t = System.currentTimeMillis();
+            selectCostOfTheProduct();
+            System.out.println("\n \n Lead time: " + (System.currentTimeMillis() - t));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException d) {
+            System.out.println("Error: there is no product with this name");
+        } finally {
+            disconnect();
+        }
     }
 
     private static void selectCostOfTheProduct() throws SQLException {
@@ -39,12 +110,6 @@ public class Main {
     private static void badSelect(String sqlQuery) throws SQLException {
         ResultSet rs = stmt.executeQuery("SELECT cost FROM shop WHERE title = 'Product 9986';");
         System.out.println("The cost of the product = " + rs.getInt(1));
-    }
-
-    private static void taskOne() throws SQLException {
-        dropTable();
-        createTable();
-        fillTable();
     }
 
     private static void fillTable() throws SQLException {
